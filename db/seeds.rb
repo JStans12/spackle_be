37140885948824d831af9beb0e9ab5e9b1f420b3
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-02 13:14:38 -0700

    changed seeds to reddit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ taylor = User.create(name: "tswift", email: "tswift@hotmail.com")
 kanye  = User.create(name: "yeezy", email: "yeezy@hotmail.com")
 donald = User.create(name: "yugehands", email: "yugehands@hotmail.com")
 
-twitter = Page.create(url: "twitter.com")
+reddit = Page.create(url: "https://www.reddit.com/")
 
-unprovoked_aggression = kanye.comment("Taylor sucks!", twitter)
-thoughtful_response   = taylor.comment("No, you suck!", twitter, unprovoked_aggression)
-presidential_grace    = donald.comment("You both suck! Sad!", twitter, thoughtful_response)
+unprovoked_aggression = kanye.comment("Taylor sucks!", reddit)
+thoughtful_response   = taylor.comment("No, you suck!", reddit, unprovoked_aggression)
+presidential_grace    = donald.comment("You both suck! Sad!", reddit, thoughtful_response)
 
 taylor.downvote(unprovoked_aggression)
 kanye.downvote(thoughtful_response)
