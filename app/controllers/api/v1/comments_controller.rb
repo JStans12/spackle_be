d3b7af7d5995f32d313f85d612a8025b477266cb
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-02 07:57:29 -0700

    can send url in headers instead of params

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::CommentsController < ApiController
 
   def index
-    page = Page.find_by(url: params[:url])
+    page = Page.find_by(url: url)
     comments = page.comments
     render json: comments
   end
+
+  private
+
+    def url
+      request.headers.env.detect{|k, _| k =~ /^HTTP_PAGE_URL/}[1]
+    end
 end
