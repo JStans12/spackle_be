2cb1b1cd06f297a6c16e7cf5fc75e2d06e2edef0
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-02 21:03:02 -0700

    comment json includes score

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :author, :parent_id, :created_at, :updated_at
+  attributes :id, :body, :author, :parent_id, :created_at, :updated_at, :score
   has_many :children
 
   def author
