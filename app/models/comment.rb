c2ea23a04e59b8e2be8e25de87ff043202d6fad8
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-03 10:13:39 -0700

    can order comments by score

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,13 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: "Comment", foreign_key: "parent_id", required: false
   has_many :children, class_name: "Comment", foreign_key: "parent_id", dependent: :destroy
 
+  def self.by_score
+    select('comments.*', 'sum(ups.value) AS score')
+    .left_outer_joins(:ups)
+    .group('id')
+    .order('score DESC')
+  end
+
   def score
     ups.sum(:value)
   end
