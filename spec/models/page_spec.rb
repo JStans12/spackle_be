42e3fd85fb11f0fbffbd1c970bccc9460cfc92f5
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-02 20:01:16 -0700

    base comments tested

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Page, type: :model do
     it { should respond_to :comments }
     it { should respond_to :commenters }
   end
+
+  describe "model methods" do
+    context "#base_comments" do
+      it "returns comments where parent_id is nil" do
+        user = create(:user)
+        page, page2 = create_list(:page, 2)
+        comment = user.comment("stuff", page)
+        comment2 = user.comment("stuff", page, comment)
+        comment3 = user.comment("stuff", page)
+        comment4 = user.comment("stuff", page2)
+
+        expect(page.base_comments).to include(comment)
+        expect(page.base_comments).to include(comment3)
+        expect(page.base_comments).to_not include(comment2)
+        expect(page.base_comments).to_not include(comment4)
+      end
+    end
+  end
 end
