0c9dbd5d64fb35c37848c117fd25b36746bf52ac
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-03 17:28:29 -0700

    api can confirm user credentials

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::SessionsController < ApiController
+
+  def new
+    user = User.find_by(name: name)
+    if user && user.authenticate(password)
+      render json: { success: "correct credentials" }
+    else
+      render json: { failure: "bad credentials" }, status: 400
+    end
+  end
+
+  private
+
+    def name
+      request.headers.env.detect{|k, _| k =~ /^HTTP_NAME/}[1]
+    end
+
+    def password
+      request.headers.env.detect{|k, _| k =~ /^HTTP_PASSWORD/}[1]
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/comments', to: 'comments#index'
+
+      get '/login', to: 'sessions#new'
     end
   end
 end
