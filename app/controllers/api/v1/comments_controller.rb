007665cb4bf099b8f8433999b140c755320d7186
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-05 10:49:39 -0700

    api can accept request to post comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,6 +6,19 @@ class Api::V1::CommentsController < ApiController
     render json: comments
   end
 
+  def create
+    user = User.find_by(token: params[:user_id])
+    page = Page.find_by(url: url)
+    parent = Comment.find(params[:parent_id]) if params[:parent_id].to_i > 0
+    body = params[:body]
+    if user
+      user.comment(body, page, parent)
+      render json: "success"
+    else
+      render json: "failure", status: 400
+    end
+  end
+
   private
 
     def url
