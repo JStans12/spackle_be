e24cc3d5d34bb811395deadf4cac32b2391f06de
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-09 14:56:12 -0700

    configure action mailer for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Setup action mailer for heroku
+  config.action_mailer.default_url_options = { :host => 'https://polar-garden-93448.herokuapp.com/' }
 end
