d3449d3a80d121ee8aba8289116f3d3db55d5680
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-10 08:48:20 -0700

    better login errors

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,14 @@ class Api::V1::SessionsController < ApiController
     if user && user.confirmed? && user.authenticate(params["password"])
       render json: user
     else
-      render json: { failure: "bad credentials" }, status: 400
+      render json: errors(user), status: 400
     end
   end
+
+  private
+
+    def errors(user)
+      return "email is not comfirmed" if user && !user.confirmed?
+      return "invalid credentials"
+    end
 end
