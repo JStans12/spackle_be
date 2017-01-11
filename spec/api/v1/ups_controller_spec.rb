4de160acf9c232b131bf887915d040fdff40f1bb
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-11 15:18:36 -0700

    ups controller alternative paths tested

--- spec/api/v1/ups_controller_spec.rb
+++ spec/api/v1/ups_controller_spec.rb
@@ -15,4 +15,34 @@ describe Api::V1::UpsController do
       expect(comment.score).to eq(-1)
     end
   end
+
+  context "create same value" do
+    it "returns a success message" do
+      reddit = Page.create(url: "https://www.reddit.com/")
+      user = create(:user)
+      user.confirmed!
+      comment = user.comment("Hey Reddit!", reddit)
+
+      post "/api/v1/users/#{user.id}/ups", params: { token: user.token, comment_id: comment.id, value: 1 }
+      message = response.body
+
+      expect(message).to eq("{\"success\":\"vote tallied\"}")
+      expect(comment.score).to eq(0)
+    end
+  end
+
+  context "create sadpath" do
+    it "returns a failure message" do
+
+      reddit = Page.create(url: "https://www.reddit.com/")
+      user = create(:user)
+      user.confirmed!
+      comment = user.comment("Hey Reddit!", reddit)
+
+      post "/api/v1/users/#{user.id}/ups", params: { token: 555, comment_id: comment.id, value: -1 }
+      message = response.body
+
+      expect(message).to eq("{\"failure\":\"invalid credentials\"}")
+    end
+  end
 end
