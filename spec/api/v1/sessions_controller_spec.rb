fb4d0aa16ab2798dcac73365c42989b2f766b5e9
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-11 15:34:33 -0700

    sessions sad paths tested

--- spec/api/v1/sessions_controller_spec.rb
+++ spec/api/v1/sessions_controller_spec.rb
@@ -13,4 +13,27 @@ describe Api::V1::SessionsController do
       expect(user_data[:email]).to eq(user.email)
     end
   end
+
+  context "create sad path unconfimred user" do
+    it "returns the users information" do
+      user = create(:user)
+
+      post '/api/v1/login', params: { name: user.name, password: "p" }
+      message = response.body
+
+      expect(response.body).to eq("email is not comfirmed")
+    end
+  end
+
+  context "create sad path bad creds" do
+    it "returns the users information" do
+      user = create(:user)
+      user.confirmed!
+
+      post '/api/v1/login', params: { name: user.name, password: "j" }
+      message = response.body
+
+      expect(response.body).to eq("invalid credentials")
+    end
+  end
 end
